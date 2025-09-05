ca289f31c5521f65d8453e04a893a6716fc452c7
金生旺 <jinshengwang@xiaohongshu.net>
2025-09-05 17:25:54 +0800

    feat: 图片功能

--- xhs/app/_layout.tsx
+++ xhs/app/_layout.tsx
@@ -2,7 +2,7 @@ import { Stack } from 'expo-router';
 
 export default function RootLayout() {
   return (
-    <Stack>
+    <Stack screenOptions={{headerStyle: {backgroundColor: '#25292e'}}}>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       <Stack.Screen name="+not-found" options={{ title: 'Not Found' }} />
     </Stack>
